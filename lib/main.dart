a3141624000d0a96f8a0b5accdc53a340460b03f
Ashutosh <ashutosh.na.99@gmail.com>
2022-11-12 02:44:28 +0530

    Added the KONTESTS API and display to show all competitions

--- lib/main.dart
+++ lib/main.dart
@@ -1,47 +1,77 @@
 import 'package:flutter/material.dart';
-//import 'package:http/http.dart' as http;
+import 'package:http/http.dart' as http;
+import 'package:url_launcher/url_launcher.dart';
+import 'dart:convert' as convert;
 import "my-account.dart";
 import "login-logout.dart";
 import "settings.dart";
 
-
-final List<int> _items = List<int>.generate(51, (int index) => index);
-const List<String> list = <String>['Top Stories', 'Newest'];
+const List<String> list = <String>['KONTESTS','Top Stories', 'Newest'];
 final List<String> entries = <String>['A Story here!', 'Button', 'Comments'];
 final List<int> colorCodes = <int>[600, 500, 100];
 
 
-void main() {
-  runApp(const AppBarApp());
-}
 
-class AppBarApp extends StatelessWidget {
-  const AppBarApp({super.key});
+void main() => runApp(MyApp());
 
+class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
+      title: 'CPCA',
       theme: ThemeData(
         colorSchemeSeed: const Color(0xff6750a4),
         useMaterial3: true,
         //primaryColor: Color(0xFF6200EE),
       ),
-      // Decide which user to login?
-      // How to choose login screen or home screen
-      home: const AppBarExample(),
+      // to hide debug banner
+      debugShowCheckedModeBanner: false,
+      home: FutureDemoPage(),
     );
   }
 }
 
-class AppBarExample extends StatefulWidget {
-  const AppBarExample({super.key});
-
+class HomePage extends StatelessWidget {
   @override
-  State<AppBarExample> createState() => _AppBarExampleState();
+  Widget build(BuildContext context) {
+    return SafeArea(
+      child: Scaffold(
+        appBar: AppBar(
+          title: Text('GeeksforGeeks'),
+        ),
+        body: Center(
+          child: ElevatedButton(
+            onPressed: () => Navigator.push(
+              context,
+              MaterialPageRoute(
+                builder: (ctx) => FutureDemoPage(),
+              ),
+            ),
+            child: Text('Demonstrate FutureBuilder'),
+          ),
+        ),
+      ),
+    );
+  }
 }
 
-class _AppBarExampleState extends State<AppBarExample> {
-  int _selectedDestination = 0;
+class FutureDemoPage extends StatelessWidget {
+  /// Function that will return a
+  /// "string" after some time
+  /// To demonstrate network call
+  /// delay of [2 seconds] is used
+  ///
+  /// This function will behave as an
+  /// asynchronous function
+  Future<String> getData() async {
+    var url = Uri.parse('https://kontests.net/api/v1/all');
+    var response = await http.get(url);
+    if (response.statusCode == 200) {
+      return response.body.toString();
+      //print({response.body});
+    }
+    return response.statusCode.toString();
+  }
   bool shadowColor = false;
   double? scrolledUnderElevation;
 
@@ -50,117 +80,149 @@ class _AppBarExampleState extends State<AppBarExample> {
     final ColorScheme colorScheme = Theme.of(context).colorScheme;
     final Color oddItemColor = colorScheme.primary.withOpacity(0.05);
     final Color evenItemColor = colorScheme.primary.withOpacity(0.15);
-    final theme = Theme.of(context);
-    final textTheme = theme.textTheme;
-    bool _pinned = true;
-    bool _snap = false;
-    bool _floating = false;
+    return SafeArea(
+      child: Scaffold(
+        appBar:AppBar(
+          title: const DropdownMenu(),
+          scrolledUnderElevation: scrolledUnderElevation,
+          shadowColor: shadowColor ? Theme.of(context).colorScheme.shadow : null,
+            actions: <Widget>[
+              IconButton(
+                icon: const Icon(Icons.search_rounded),
+                tooltip: 'Search',
+                onPressed: () {
+                  ScaffoldMessenger.of(context).showSnackBar(
+                      const SnackBar(content: Text('Search: Not implemented yet')));
+                },
+              ),
+              PopupMenuButton(
+                // add icon, by default "3 dot" icon
+                // icon: Icon(Icons.book)
+                  itemBuilder: (context){
+                    return [
+                      const PopupMenuItem<int>(
+                        value: 0,
+                        child: Text("My Account"),
+                      ),
 
+                      const PopupMenuItem<int>(
+                        value: 1,
+                        child: Text("Settings"),
+                      ),
 
-    return Scaffold(
-      body: CustomScrollView(
-        slivers: <Widget>[
-          SliverAppBar(
-            pinned: false,
-            snap: true,
-            floating: true,
-            expandedHeight: 100.0,
-            toolbarHeight: 100.0,
-            //flexibleSpace: const FlexibleSpaceBar(
-              title: const DropdownButtonExample(),
-              actions: <Widget>[
-                IconButton(
-                  icon: const Icon(Icons.search_rounded),
-                  tooltip: 'Search',
-                  onPressed: () {
-                    ScaffoldMessenger.of(context).showSnackBar(
-                        const SnackBar(content: Text('Search: Not implemented yet')));
+                      const PopupMenuItem<int>(
+                        value: 2,
+                        child: Text("Logout"),
+                      ),
+                    ];
                   },
-                ),
-                PopupMenuButton(
-                  // add icon, by default "3 dot" icon
-                  // icon: Icon(Icons.book)
-                    itemBuilder: (context){
-                      return [
-                        PopupMenuItem<int>(
-                          value: 0,
-                          child: Text("My Account"),
-                        ),
-
-                        PopupMenuItem<int>(
-                          value: 1,
-                          child: Text("Settings"),
-                        ),
-
-                        PopupMenuItem<int>(
-                          value: 2,
-                          child: Text("Logout"),
-                        ),
-                      ];
-                    },
-                    onSelected:(value){
-                      if(value == 0){
-                        Navigator.push(
-                            context,
-                            new MaterialPageRoute(builder: (context) => new AccountPage()),
-                        );
-                      } else if(value == 1){
-                        Navigator.push(
-                          context,
-                          new MaterialPageRoute(builder: (context) => new SettingsScreen()),
-                        );
-                      }else if(value == 2){
-                        Navigator.push(
-                          context,
-                          new MaterialPageRoute(builder: (context) => new LoginLogoutScreen()),
-                        );
-                      }
-                    }
-                ),
-              ]
-            //),
-          ),
-          SliverList(
-            delegate: SliverChildBuilderDelegate(
-                  (BuildContext context, int index) {
-                return Container(
-                  alignment: Alignment.center,
-                  // tileColor: _items[index].isOdd ? oddItemColor : evenItemColor,
-                  //margin: const EdgeInsets.symmetric(vertical: 00.0),
-                  height: 100.0,
-                  child: ListView.separated(
-                      scrollDirection: Axis.horizontal,
-                    padding: const EdgeInsets.all(8),
-                    itemCount: entries.length,
-                    itemBuilder: (BuildContext context, int index) {
-                      return Container(
-                        height: 50,
-                        color: Colors.amber[colorCodes[index]],
-                        child: Center(child: Text('${entries[index]}')),
+                  onSelected:(value){
+                    if(value == 0){
+                      Navigator.push(
+                        context,
+                        MaterialPageRoute(builder: (context) => AccountPage()),
                       );
-                    },
-                    separatorBuilder: (BuildContext context, int index) => const Divider(),
+                    } else if(value == 1){
+                      Navigator.push(
+                        context,
+                        MaterialPageRoute(builder: (context) => SettingsScreen()),
+                      );
+                    }else if(value == 2){
+                      Navigator.push(
+                        context,
+                        MaterialPageRoute(builder: (context) => LoginLogoutScreen()),
+                      );
+                    }
+                  }
+              ),
+            ]
+        ),
+        body: FutureBuilder(
+          builder: (ctx, snapshot) {
+            // Checking if future is resolved or not
+            if (snapshot.connectionState == ConnectionState.done) {
+              // If we got an error
+              if (snapshot.hasError) {
+                return Center(
+                  child: Text(
+                    '${snapshot.error} occurred',
+                    style: TextStyle(fontSize: 18),
                   ),
                 );
-              },
-              childCount: 20,
-            ),
-          ),
 
-        ],
+                // if we got our data
+              } else if (snapshot.hasData) {
+                // Extracting data from snapshot object
+                final data = snapshot.data as String;
+                var jsonResponse = convert.jsonDecode(data);
+                final List<int> _items = List<int>.generate(jsonResponse.length, (int index) => index);
+                return GridView.builder(
+                  shrinkWrap: true,
+                  itemCount: jsonResponse.length,
+                  padding: const EdgeInsets.all(8.0),
+                  gridDelegate: const SliverGridDelegateWithFixedCrossAxisCount(
+                    crossAxisCount: 1,
+                    childAspectRatio: 5.0,
+                    mainAxisSpacing: 10.0,
+                    crossAxisSpacing: 10.0,
+                  ),
+                  itemBuilder: (BuildContext context, int index) {
+                    return InkWell(
+                      child: Container(
+                        alignment: Alignment.center,
+                        decoration: BoxDecoration(
+                          borderRadius: BorderRadius.circular(10.0),
+                          color: _items[index].isOdd ? oddItemColor : evenItemColor,
+                        ),
+                        child: Column(
+                            mainAxisAlignment: MainAxisAlignment.center,
+                            children:[
+                              Row(children:[
+                                Flexible(child: Text(jsonResponse[index]['name'], style: const TextStyle(fontWeight: FontWeight.bold, fontSize: 17))),
+                              ]),
+                              Row(children:[
+                                Flexible(child: Text(jsonResponse[index]['site'], style: const TextStyle(fontWeight: FontWeight.bold, fontSize: 14))),
+                              ])
+                            ]),
+                      ),
+                      onTap: () async {
+                        var compUrl = Uri.parse(jsonResponse[index]['url']);
+                        if (await canLaunchUrl(compUrl)) {
+                          await launchUrl(compUrl,  mode: LaunchMode.externalApplication);
+                        } else {
+                          ScaffoldMessenger.of(context).showSnackBar(
+                              const SnackBar(content: Text('Error has occurred')));
+                        }
+                      },
+                    );
+                  },
+                );
+              }
+            }
+
+            // Displaying LoadingSpinner to indicate waiting state
+            return Center(
+              child: CircularProgressIndicator(),
+            );
+          },
+
+          // Future that needs to be resolved
+          // inorder to display something on the Canvas
+          future: getData(),
+        ),
       ),
     );
   }
 }
 
-class DropdownButtonExample extends StatefulWidget {
-  const DropdownButtonExample({super.key});
+class DropdownMenu extends StatefulWidget {
+  const DropdownMenu({super.key});
 
   @override
-  State<DropdownButtonExample> createState() => _DropdownButtonExampleState();
+  State<DropdownMenu> createState() => _DropdownMenuState();
 }
 
-class _DropdownButtonExampleState extends State<DropdownButtonExample> {
+class _DropdownMenuState extends State<DropdownMenu> {
   String dropdownValue = list.first;
 
   @override
@@ -184,7 +246,7 @@ class _DropdownButtonExampleState extends State<DropdownButtonExample> {
         return DropdownMenuItem<String>(
           value: value,
           child: Text(value,
-            style: const TextStyle(fontWeight: FontWeight.bold, fontSize: 25)),
+              style: const TextStyle(fontWeight: FontWeight.bold, fontSize: 25)),
 
         );
       }).toList(),

95aca5a8f80b20eb22d33319f680276ca5dd337f
Ashutosh <ashutosh.na.99@gmail.com>
2022-11-13 18:19:03 +0530

    Manifest prep for release

--- lib/secrets.dart
+++ lib/secrets.dart
@@ -1,5 +1,5 @@
 class Secrets {
   static const testEmail = 'test@test.com';
   static const testPassword = 'test-password';
-  static const pocketbase_url = 'https://07f4-171-76-80-148.in.ngrok.io';
+  static const pocketbase_url = 'https://e95b-171-76-80-148.in.ngrok.io';
 }

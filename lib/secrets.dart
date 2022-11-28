4a6ec2db88b4af3f2271031b1bdf9fcfc11e85fe
Ashutosh <ashutosh.na.99@gmail.com>
2022-11-28 19:22:27 +0530

    Final update

--- lib/secrets.dart
+++ lib/secrets.dart
@@ -1,5 +1,5 @@
 class Secrets {
   static const testEmail = 'test@test.com';
   static const testPassword = 'test-password';
-  static const pocketbase_url = 'https://35c7-61-12-83-162.in.ngrok.io';
+  static const pocketbase_url = 'https://61a9-171-76-87-70.in.ngrok.io';
 }
